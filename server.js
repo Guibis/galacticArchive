f932fb533091bf1ee9a878b7881e6cbea50f9e84
guibis <guilhermeplima5@gmail.com>
2026-01-21 14:03:11 -0300

    Feat: functions for reading and writing using fs and the get endpoint.

--- server.js
+++ server.js
@@ -5,8 +5,22 @@ const port = 3000;
 app.use(express.json());
 const archive_file = './archive.json';
 
+const readArchive = () => {
+    try {
+        const data = fs.readFileSync(archive_file);
+        return JSON.parse(data);
+    } catch (error) {
+        return [];
+    }
+};
+
+const writeArchive = (data) => {
+    fs.writeFileSync(archive_file, JSON.stringify(data));
+};
+
 app.get('/archive', (req, res) => {
-    res.send('Hello World!');
+    const archive = readArchive();
+    res.json(archive);
 });
 
 app.listen(port, () => {
