59f4b424513b6e17d803dc835a1bb7bd10b11210
guibis <guilhermeplima5@gmail.com>
2026-01-21 14:45:05 -0300

    feat: Add request logging and global error handling middleware.

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const fs = require('fs');
 const port = 3000;
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
 const archive_file = './archive.json';
 
 const readArchive = () => {
@@ -69,6 +74,11 @@ app.delete('/archive/:id', (req, res) => {
     archive.splice(archive.indexOf(item), 1);
     writeArchive(archive);
     res.json(item);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 app.listen(port, () => {
