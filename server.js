4dbe8d26cedde6e7cfe861cc1295015c097cdf82
guibis <guilhermeplima5@gmail.com>
2026-01-21 18:22:18 -0300

    feat: Implement a full-stack web application for managing Galactic Archive entries with CRUD functionality and a LCARS-inspired UI.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const fs = require('fs');
 const port = 3000;
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
