c13bbd46306edf8db1f08a3383a4b0a3eed77499
guibis <guilhermeplima5@gmail.com>
2026-01-21 13:58:45 -0300

    feat: Initialize Node.js Express project with basic server, dependencies, and .gitignore.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const port = 3000;
+app.use(express.json());
+const archive_file = './archive.json';
+
+app.get('/archive', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
